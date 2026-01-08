56bc7d6ce07bb89bfc268e7610d3b475717fae27
Nivedck <achuchakkara19@gmail.com>
2026-01-08 22:53:23 +0530

    UI Theme modified . Added Light/Dark Modes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import Head from 'next/head';
 import '../public/styles.css';
+import Layout from '../lib/components/Layout';
+import { ThemeProvider } from '../lib/context/ThemeContext';
 
 export default function App({ Component, pageProps }){
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <meta name="theme-color" content="#0a3d62" />
+        <meta name="theme-color" content="#2563eb" />
         <title>Makerspace Access</title>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
